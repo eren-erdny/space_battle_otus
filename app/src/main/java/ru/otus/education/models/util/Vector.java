378065eaaa98cdb066c96bbe99146c6fceab21e7
enerendzhenov <eren.erdny@gmail.com>
2024-02-13 20:33:42 +0300

    Added multidimensional double Vector.java

--- app/src/main/java/ru/otus/education/models/util/Vector.java
+++ app/src/main/java/ru/otus/education/models/util/Vector.java
@@ -1,33 +1,42 @@
 package ru.otus.education.models.util;
 
+import java.util.Arrays;
+
+import static java.lang.Math.min;
+
 public class Vector {
+    double [] coordinates;
 
-    double x;
-    double y;
-
-    public Vector() {
-    }
-
-    public Vector(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public Vector(double...coordinates) {
+        this.coordinates = coordinates;
     }
 
     public static Vector plus(Vector vector1, Vector vector2)  {
-        Vector result = new Vector();
-        result.x = vector1.x + vector2.x;
-        result.y = vector1.y + vector2.y;
-        return result;
+
+        if (vector1 == null || vector2 == null) {
+            throw new RuntimeException("Null vector passed");
+        }
+
+        if (vector2.coordinates.length != vector1.coordinates.length) {
+            throw new RuntimeException("Cannot add vectors with different dimensions");
+        }
+
+        return sequentialAddition(vector1.coordinates, vector2.coordinates);
     }
 
-    public static Vector minus(Vector vector1, Vector vector2) {
-        Vector result = new Vector();
-        result.x = vector1.x + vector2.x;
-        result.y = vector1.y + vector2.y;
-        return result;
+    private static Vector sequentialAddition(double[] array1, double[] array2) {
+        var resultCoordinates = new double[min(array1.length, array2.length)];
+
+        if (resultCoordinates.length == 0) {
+            throw new RuntimeException("Zero dimension vector passed");
+        }
+        for (int i = 0; i < array1.length; i++) {
+            resultCoordinates[i] = array1[i] + array2[i];
+        }
+        return new Vector(resultCoordinates);
     }
 
     public static boolean equal(Vector vector1, Vector vector2) {
-        return vector1.x == vector2.x && vector1.y == vector2.y;
+        return Arrays.equals(vector1.coordinates, vector2.coordinates);
     }
 }
